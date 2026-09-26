3850c9911c9da4e1eca21e281a1bbb63e3a876a8
Alexei Zhukevich <j.zhukevich.aa@gmail.com>
2026-09-26 17:39:01 +0300

    feat: add test pages route
    fix: return mistakely removed index.php

--- config/routes.php
+++ config/routes.php
@@ -4,8 +4,17 @@ use App\Http\Router;
 use App\Http\Request;
 use App\Http\Response;
 
+use App\Lib\View;
+
 return function(Router $router)
 {
+    $router->get('/pages/test', function (Request $request){
+        $page = $request->getQuery('page');
+        if (!is_string($page)) return Response::html("<p>Provide page name in ?page query</p>");
+
+        return Response::html(View::render($page, $request->query));
+    }, 'test_pages');
+
     $router->get('/test', function (Request $request) {
         return Response::html("<h1>Test completed successfully</h1>");
     }, 'test_route');
