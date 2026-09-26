c4407e6eb74c479707b9e4df7895a1dbf2d45102
Maksim Shyshko <lamakswork@gmail.com>
2026-09-26 18:44:15 +0300

    layout: connect head_extra block in main.php

--- src/layouts/main.php
+++ src/layouts/main.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="/assets/css/base.css">
 <link rel="stylesheet" href="/assets/css/layout.css">
 <link rel="stylesheet" href="/assets/css/component.css">
+<?= $view->block('head_extra') ?>
 </head>
 <body>
 
